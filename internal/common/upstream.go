8d0bd49a3f6486d4cc255cb2b3865f094474bdf9
stydxm <stydxm@outlook.com>
2025-07-01 19:37:07 +0800

    feat: match2video handler

--- internal/common/upstream.go
+++ internal/common/upstream.go
@@ -7,7 +7,8 @@ const (
 )
 
 const (
-	UpstreamUrlGroupRankInfo = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/group_rank_info.json"
-	UpstreamUrlRobotData     = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/robot_data.json"
-	UpstreamUrlSchedule      = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/schedule.json"
+	UpstreamUrlGroupRankInfo       = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/group_rank_info.json"
+	UpstreamUrlRobotData           = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/robot_data.json"
+	UpstreamUrlSchedule            = "https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/schedule.json"
+	UpstreamUrlBilibiliCollections = "https://api.bilibili.com/x/polymer/web-space/seasons_series_list?mid=20554233&page_size=20&page_num=%d"
 )
---
+++ internal/job/bilibili/bilibili_parsers.go
@@ -0,0 +1,100 @@
+package bilibili
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/scutrobotlab/rm-schedule/internal/common"
+	"github.com/scutrobotlab/rm-schedule/internal/router"
+	"github.com/scutrobotlab/rm-schedule/internal/static"
+	"github.com/scutrobotlab/rm-schedule/internal/svc"
+	"github.com/scutrobotlab/rm-schedule/internal/types"
+	"log"
+	"strings"
+)
+
+var schedules = map[string][]byte{
+	"2024": static.ScheduleBytes2024,
+	"2025": router.RedirectParams[common.UpstreamNameSchedule].Data,
+}
+
+type Matches map[string]map[string][]types.MatchNode
+
+func getMatches() Matches {
+	matches := Matches{}
+
+	if !router.RedirectParams[common.UpstreamNameSchedule].Static {
+		liveScheduleData, ok := svc.Cache.Get(router.RedirectParams[common.UpstreamNameSchedule].Name)
+		if ok {
+			schedules["2025"] = liveScheduleData.([]byte)
+		}
+	}
+
+	for season, scheduleBytes := range schedules {
+		var scheduleData types.ScheduleResp
+		err := json.Unmarshal(scheduleBytes, &scheduleData)
+		if err != nil {
+			log.Printf("failed to unmarshal schedule: %v\n", err)
+			return nil
+		}
+
+		zones := make(map[string][]types.MatchNode)
+		for _, currentZone := range scheduleData.Data.Event.Zones.Nodes {
+			var currentMatches []types.MatchNode
+			for _, match := range currentZone.GroupMatches.Nodes {
+				currentMatches = append(currentMatches, match)
+			}
+			for _, match := range currentZone.KnockoutMatches.Nodes {
+				currentMatches = append(currentMatches, match)
+			}
+			zones[currentZone.Name] = currentMatches
+		}
+		matches[season] = zones
+	}
+
+	return matches
+}
+
+func findCollection(season string, zone string, collectionList *[]types.BiliBiliCollectionMetaData) (types.BiliBiliCollectionMetaData, bool) {
+	for _, collection := range *collectionList {
+		collectionName := collection.Name
+		isReplay := strings.Contains(collectionName, "比赛回放") || strings.Contains(collectionName, "直播回放")
+		isRMUC := strings.Contains(collectionName, "RMUC") || strings.Contains(collectionName, "超级对抗赛")
+		isCorrectSeason := strings.Contains(collectionName, season)
+		var isCorrectZone bool
+		if len([]rune(zone)) > 3 {
+			isCorrectZone = strings.Contains(collectionName, string([]rune(zone)[:3])) //港澳台及海外赛区&复活赛在赛程中被拆分为两段，回放中属于同一合集
+		} else {
+			isCorrectZone = strings.Contains(collectionName, zone)
+		}
+		if isReplay && isRMUC && isCorrectSeason && isCorrectZone {
+			return collection, true
+		}
+	}
+	return types.BiliBiliCollectionMetaData{Name: "not found", CollectionId: 0}, false
+}
+
+func checkStringInclude(fullStr string, subStr string) bool {
+	var isInclusion bool
+	if len([]rune(subStr)) > 3 {
+		isInclusion = strings.Contains(fullStr, string([]rune(subStr)[:3])) || strings.Contains(fullStr, string([]rune(subStr)[3:]))
+	} else {
+		isInclusion = strings.Contains(fullStr, subStr)
+	}
+	return isInclusion
+}
+
+func findMatchVideo(match *types.MatchNode, collection *types.BiliBiliCollectionInfo) (types.BiliBiliVideoMetaData, bool) {
+	for _, video := range collection.Data.Archives {
+		videoTitle := video.Title
+		isCorrectOrderNum := strings.Contains(videoTitle, fmt.Sprintf("第%d场", match.OrderNumber)) ||
+			strings.Contains(videoTitle, fmt.Sprintf("第 %d 场", match.OrderNumber))
+		isCorrectBlueTeam := checkStringInclude(videoTitle, match.BlueSide.Player.Team.Name)
+		isCorrectBlueSchool := checkStringInclude(videoTitle, match.BlueSide.Player.Team.CollegeName)
+		isCorrectRedTeam := checkStringInclude(videoTitle, match.RedSide.Player.Team.Name)
+		isCorrectRedSchool := checkStringInclude(videoTitle, match.RedSide.Player.Team.CollegeName)
+		if isCorrectOrderNum && isCorrectBlueTeam && isCorrectBlueSchool && isCorrectRedTeam && isCorrectRedSchool {
+			return video, true
+		}
+	}
+	return types.BiliBiliVideoMetaData{}, false
+}
